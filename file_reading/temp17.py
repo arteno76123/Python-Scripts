166acfd8d97848921f790fd53ed39e3fb0226277
arteno76123 <alexander.kubani@gmail.com>
2023-01-17 08:48:25 +0100

    Create temp17.py

---
+++ file_reading/temp17.py
@@ -0,0 +1,14 @@
+with open("book1.csv", encoding = "utf-8") as source:
+    for line in source:
+        line = line.strip()
+        line = line.replace('\ufeff', '')
+        line = line.split(",")
+        line = [int(i) for i in line]
+       
+        print(sum(line)/len(line))
+        
+
+    
+    
+   
+    

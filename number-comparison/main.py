f1a5667a13324e11ce01763875603abaeb1d969f
arteno76123 <alexander.kubani@gmail.com>
2023-01-24 09:05:39 +0100

    Update main.py

--- number-comparison/main.py
+++ number-comparison/main.py
@@ -1,10 +1,13 @@
-number1 = int(input("Enter the first number: "))
-number2 = int(input("Enter the second number: "))
-
-if number1 < number2:
-    print(f"{number2} is greater than {number1}.")
-elif number1 > number2:
-    print(f"{number1} is greater than {number2}.")
-else:
-    print("Both are equal.")
+try:
+    number1 = int(input("Enter the first number: "))
+    number2 = int(input("Enter the second number: "))
+except ValueError:
+    print("One of the entered values does not look like an integer.")
+else:    
+    if number1 < number2:
+        print(f"{number2} is greater than {number1}.")
+    elif number1 > number2:
+        print(f"{number1} is greater than {number2}.")
+    else:
+        print("Both are equal.")
 
